890c39ae85a3c7af14de6d1fac2bb2f5c28a7870
lillian1015 <lillianlee.n@gmail.com>
2017-02-17 16:55:33 -0800

    firstPrototype

---
+++ 12Days_FinalScene/Assets/PersonAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.ColliderEvent;
+using HTC.UnityPlugin.Utility;
+using HTC.UnityPlugin.Vive;
+
+public class PersonAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Physics.OverlapBox ();		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Sword")
+		{
+			Debug.Log ("DIED");
+		}
+	}
+
+
+
+
+}
---
+++ 12Days_FinalScene/Assets/SwordAction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAction : MonoBehaviour {
+
+	public GameObject model;
+	public LayerMask layer;
+	public GameObject humanExplodeEffect;
+	public float explodeForce;
+	// Use this for initialization
+	void Start () {
+		humanExplodeEffect.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() 
+	{
+		Vector3 fwd =gameObject.transform.forward;
+		RaycastHit hit;
+		if (Physics.Raycast (model.transform.position, fwd, out hit, 1f, layer))
+		{
+			if (hit.collider.gameObject.name == "Person")
+			{
+				Debug.Log (hit.collider.gameObject.name);
+				Destroy (hit.collider.gameObject);
+				humanExplodeEffect.SetActive (true);
+				Vector3 explodePos = hit.point;
+				//collection in raduis
+				Collider[] colliders = Physics.OverlapSphere (explodePos,2f);
+				foreach(Collider coll in colliders){
+					Rigidbody rb = coll.GetComponent <Rigidbody> ();
+					if (rb != null)
+						//Force, explodeRaduis, Modifer
+						rb.AddExplosionForce (explodeForce, explodePos, 2f, 3f);
+				}
+
+
+
+			}
+		}
+	}
+
+}
